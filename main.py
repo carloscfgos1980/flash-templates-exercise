ca972cb330923ea2ed376dc07fd789e0383f8d84
Carlos Infante <kuva5008@gmail.com>
2023-01-17 06:13:42 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for
+
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/home/")
+def home():
+    return redirect(url_for('index'))
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+'''
+@app.route("/home")
+def home():
+    return render_template('index.html')
+'''
+
+if __name__ == "__main__":
+    app.run()
